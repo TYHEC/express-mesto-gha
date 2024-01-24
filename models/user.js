e99d73786f5ff4a76471072f7d89f7e44049cc6a
Artem Akulov <oknerahaz@yandex.ru>
2024-01-24 21:49:46 +0300

    fix

--- models/user.js
+++ models/user.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Имя должно быть заполнено'],
-    minlength: [2, 'Имя не может быть короче 2 символов'],
-    maxlength: [30, 'Имя не может быть длиннее 30 символов'],
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   about: {
     type: String,
-    required: [true, 'Информация о себе должна быть заполнена'],
-    minlength: [2, 'Информация о себе не может быть короче 2 символов'],
-    maxlength: [30, 'Информация о себе не может быть длиннее 30 символов'],
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   avatar: {
     type: String,
-    required: [true, 'Ссылка на аватар должна быть заполнена'],
+    required: true,
   },
 });
 
